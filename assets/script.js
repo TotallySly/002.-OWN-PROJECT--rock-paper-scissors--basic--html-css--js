f0a7c3c088f1ab5fb69e4fd32c977420e6e1b15f
TotallySly <07slyon@gmail.com>
2021-11-14 14:38:16 +0100

    Add functions to script file

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+function runGame() {}
+
+function checkAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displaySportQuestions() {}
+
+function displayMusicQuestion() {}
+
+function displayFilmQuestions() {}
+
+function displayRandomQuestions() {}
