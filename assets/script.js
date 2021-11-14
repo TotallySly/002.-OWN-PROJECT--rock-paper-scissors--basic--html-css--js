d50d3604e889ef41b0c6607c15d7ae4912483240
TotallySly <07slyon@gmail.com>
2021-11-14 20:28:03 +0100

    Try to change questions to not randomly generated. Failed. Done for the day

--- assets/script.js
+++ assets/script.js
@@ -102,6 +102,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let randomSportQuestion = Math.floor(Math.random() * sportQuestions.length);
     let showRandomSportQuestion = sportQuestions[randomSportQuestion];
     question.innerHTML = "<p>" + showRandomSportQuestion.question + "</p>";
+    answerA.innerHTML = showRandomSportQuestion.a;
+    answerB.innerHTML = showRandomSportQuestion.b;
+    answerC.innerHTML = showRandomSportQuestion.c;
+    answerD.innerHTML = showRandomSportQuestion.d;
 
     console.log(sportQuestions[randomSportQuestion]);
   }
