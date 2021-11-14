c595215b9dc73d392909a07b1b90d80d43006817
TotallySly <07slyon@gmail.com>
2021-11-14 15:01:59 +0100

    Add Event Listeners to Buttons after DOM has loaded

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
+//Wait for the DOM to finish loading
+// Get the button elements and add event listeners to them
+// Code Taken from Code Institute's 'Love Maths'
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
 function runGame() {}
 
 function checkAnswer() {}
