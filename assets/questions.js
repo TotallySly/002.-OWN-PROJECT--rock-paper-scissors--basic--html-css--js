a0c9592b7b19bc7530c24d1dc2e2bc84eed0adb3
TotallySly <07slyon@gmail.com>
2021-11-16 17:24:24 +0100

    Add questions script to javascript

---
+++ assets/questions.js
@@ -0,0 +1,87 @@
+// A file for all my questions.
+
+const quizQuestions = [
+  {
+    question:
+      "In which year did Denmark win the football European Championships?",
+    a: "1992",
+    b: "1996",
+    c: "1988",
+    d: "2006",
+    correctAnswer: "a",
+  },
+  {
+    question: "In which sport do they compete for the Stanley Cup?",
+    a: "Sailing",
+    b: "Ice Hockey",
+    c: "Golf",
+    d: "Horse Racing",
+    correctAnswer: "b",
+  },
+  {
+    question: "The Derby is a horse race held in which English town?",
+    a: "Epsom",
+    b: "Aintree",
+    c: "Cheltenham",
+    d: "Ascot",
+    correctAnswer: "a",
+  },
+  {
+    question: "What is the maximum break one can make in Snooker?",
+    a: "150",
+    b: "149",
+    c: "148",
+    d: "147",
+    correctAnswer: "d",
+  },
+  {
+    question:
+      "In which year did Andy Murray win his first Wimbledon Tennis Title ",
+    a: "2012",
+    b: "2013",
+    c: "2014",
+    d: "2015",
+    correctAnswer: "b",
+  },
+  {
+    question: "How many Olympic gold medals has Usain Bolt won? ",
+    a: "7",
+    b: "8",
+    c: "9",
+    d: "10",
+    correctAnswer: "b",
+  },
+  {
+    question: "Who won the first ever FIFA Woman's World Cup?",
+    a: "United States",
+    b: "Sweden",
+    c: "Norway",
+    d: "Brazil",
+    correctAnswer: "a",
+  },
+  {
+    question: "The heptathlon is made up of how many events?",
+    a: "6",
+    b: "7",
+    c: "8",
+    d: "9",
+    correctAnswer: "b",
+  },
+  {
+    question: "In tennis, which word is used for the score of zero?",
+    a: "Hate",
+    b: "Zero",
+    c: "Nul points",
+    d: "Love",
+    correctAnswer: "d",
+  },
+  {
+    question:
+      "Which football team, in 2003/04, won the English Premier League without losing a single game?",
+    a: "Chelsea",
+    b: "Manchester United",
+    c: "Arsenal",
+    d: "Liverpool",
+    correctAnswer: "c",
+  },
+];
