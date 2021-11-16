58faf59e06f375ed57cd82733a202932bb0224b8
TotallySly <07slyon@gmail.com>
2021-11-16 17:31:33 +0100

    created variables and targeted elements by id

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const currentQuestion = 0;
   const score = 0;
+  const totalQuestions = quizQuestions.length;
+
+  const quizContainer = document.getElementById("quizContainer");
+  const question = document.getElementById("question");
+  const option1 = document.getElementById("option1");
+  const option2 = document.getElementById("option2");
+  const option3 = document.getElementById("option3");
+  const option4 = document.getElementById("option4");
 });
