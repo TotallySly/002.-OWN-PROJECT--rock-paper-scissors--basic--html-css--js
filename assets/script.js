871d0bbcc30d49e728d9670aa6d8ddeb5a7079f6
TotallySly <07slyon@gmail.com>
2021-11-16 17:46:00 +0100

    add display question function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // All the variables required that will always be required.
+
   const currentQuestion = 0;
   const score = 0;
   const totalQuestions = quizQuestions.length;
+
+  // Retrieving the elements from the HTML so we can access them within the DOM
 
   const quizContainer = document.getElementById("quizContainer");
   const question = document.getElementById("question");
@@ -11,4 +15,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const option4 = document.getElementById("option4");
   const nxtButton = document.getElementById("nextQuestion");
   const result = document.getElementById("score");
+
+  // Function to display the questions within the HTML document.
+
+  function displayQuestion(questionIndex) {
+    let quest = quizQuestions(questionIndex);
+    question.textContent = questionIndex + 1 + "." + quest.question;
+    option1.textContent = quest.a;
+    option2.textContent = quest.b;
+    option3.textContent = quest.c;
+    option4.textContent = quest.d;
+  }
 });
