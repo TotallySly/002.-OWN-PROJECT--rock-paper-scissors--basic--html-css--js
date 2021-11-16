953a1dc5ba08b5ed7f01dfe6a40766ff174aee9f
TotallySly <07slyon@gmail.com>
2021-11-16 17:58:12 +0100

    Add notations to scrip to explain what is happening

--- assets/script.js
+++ assets/script.js
@@ -9,22 +9,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const quizContainer = document.getElementById("quizContainer");
   const question = document.getElementById("question");
-  const option1 = document.getElementById("option1");
-  const option2 = document.getElementById("option2");
-  const option3 = document.getElementById("option3");
-  const option4 = document.getElementById("option4");
+  const optionA = document.getElementById("optionA");
+  const optionB = document.getElementById("optionB");
+  const optionC = document.getElementById("optionC");
+  const optionD = document.getElementById("optionD");
   const nxtButton = document.getElementById("nextQuestion");
   const result = document.getElementById("score");
 
-  // Function to display the questions within the HTML document.
+  // Function to display the questions within the HTML document. The quest variable is retrieving the different indices from the questions.js script and this will be displayed within the HTML by using getElementById("question").
+
+  // The options are being retried from the questions script. with quest.a relating the the first option and this then being displayed within the html by targeting the ID of optionA, and so forth with the other options.
 
   function displayQuestion(questionIndex) {
     let quest = quizQuestions(questionIndex);
     question.textContent = questionIndex + 1 + "." + quest.question;
-    option1.textContent = quest.a;
-    option2.textContent = quest.b;
-    option3.textContent = quest.c;
-    option4.textContent = quest.d;
+    optionA.textContent = quest.a;
+    optionB.textContent = quest.b;
+    optionC.textContent = quest.c;
+    optionD.textContent = quest.d;
   }
 
   button.addEventListener("click", () => displayNextQuestion());
