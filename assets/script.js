ec7cca424e5aac0c30b7ac4d49c16021a61e55b2
TotallySly <07slyon@gmail.com>
2021-11-17 13:15:11 +0100

    Add divs to HTML and variables to script

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  // Variables for the Quiz
+  const quizContainer = (document.getElementById = "quiz");
+  const resultsContainer = (document.getElementById = "results");
+  const submitButton = (documen.getElementById = "submit");
+});
