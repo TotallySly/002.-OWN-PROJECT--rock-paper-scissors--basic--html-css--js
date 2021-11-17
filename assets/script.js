b530f89377900862d49b0b52ad200b2d15b0cdfb
TotallySly <07slyon@gmail.com>
2021-11-17 13:17:27 +0100

    Created functions

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   // Variables for the Quiz
   const quizContainer = (document.getElementById = "quiz");
   const resultsContainer = (document.getElementById = "results");
-  const submitButton = (documen.getElementById = "submit");
+  const submitButton = (document.getElementById = "submit");
+
+  function buildQuiz() {}
+
+  function showResults() {}
+
+  //   DIsplays functions right away.
+  buildQuiz();
+
+  //   On submit, show the results
+  submitButton.addEventListener("click", showResults);
 });
