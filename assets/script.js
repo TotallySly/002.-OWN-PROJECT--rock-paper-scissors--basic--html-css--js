3c977efd90e6cbe97364eaa6e8d63c95a5a70bbf
TotallySly <07slyon@gmail.com>
2021-11-17 11:45:33 +0100

    Changing indents to make script more readable

--- assets/script.js
+++ assets/script.js
@@ -1,127 +1,151 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const quizQuestions = [
+const quizQuestions = [
     {
-      question:
-        "For which movie did Steven Spielberg win his first Oscar for Best Director?",
-      a: "E.T.",
-      b: "Schindler's List",
-      c: "Jaws",
-      d: "Jurassic Park",
+    question:
+        "For which movie did Steven Spielberg win his first Oscar for Best Director?", 
+        answers: {
+    a: "E.T.",
+    b: "Schindler's List",
+    c: "Jaws",
+    d: "Jurassic Park"
     },
-      correctAnswer: "b",
+		correctAnswer: "b"
     },
     {
-      question:
+    question:
         "Which film won an Oscar for Best Makeup and Hairstyling despite having a budget of $250",
-      a: "Dallas Buyers Club",
-      b: "The Blair Witch Project",
-      c: "Paranormal Activity",
-      d: "Lord of the Rings",
+        answers: {
+    a: "Dallas Buyers Club",
+    b: "The Blair Witch Project",
+    c: "Paranormal Activity",
+    d: "Lord of the Rings"
     },
-      correctAnswer: "a",
-    },
-    {
-      question: "Who played Private Ryan in Saving Private Ryan",
-      a: "Tom Sizemore",
-      b: "Edward Burns",
-      c: "Tom Hanks",
-      d: "Matt Damon",
-    },
-      correctAnswer: "d",
+    correctAnswer: "a"
     },
     {
-      question: "In The Big Lebowski, which band does The Dude hate?",
-      a: "The Beetles",
-      b: "Eagles",
-      c: "The Rolling Stones",
-      d: "Steely Dan",
+    question: "Who played Private Ryan in Saving Private Ryan",
+    answers: {
+    a: "Tom Sizemore",
+    b: "Edward Burns",
+    c: "Tom Hanks",
+    d: "Matt Damon"
     },
-      correctAnswer: "b",
-    },
-    {
-      question: "Helena Bonham Carter and Meat Loaf appear in which film?",
-      a: "Fight Club",
-      b: "The Rocky Horror Picture Show",
-      c: "Rocky",
-      d: "Waynes World",
-    },
-      correctAnswer: "a",
+    correctAnswer: "d"
     },
     {
-      question: "On which planet was Superman born?",
-      a: "Saturn",
-      b: "Mars",
-      c: "Kypton",
-      d: "Earth",
+    question: "In The Big Lebowski, which band does The Dude hate?",
+    answers: {
+    a: "The Beetles",
+    b: "Eagles",
+    c: "The Rolling Stones",
+    d: "Steely Dan"
     },
-      correctAnswer: "c",
-    },
-    {
-      question: "Who directed th 1997 film Jackie Brown?",
-      a: "Tim Burton",
-      b: "James Cameron",
-      c: "Steven Spielberg",
-      d: "Quentin Taratino",
-    },
-      correctAnswer: "d",
+    correctAnswer: "b"
     },
     {
-      question:
+    question: "Helena Bonham Carter and Meat Loaf appear in which film?",
+    answers: {
+    a: "Fight Club",
+    b: "The Rocky Horror Picture Show",
+    c: "Rocky",
+    d: "Waynes World",
+    },
+    correctAnswer: "a",
+    },
+    {
+    question: "On which planet was Superman born?",
+    answers: {
+    a: "Saturn",
+    b: "Mars",
+    c: "Kypton",
+    d: "Earth",
+    },
+    correctAnswer: "c",
+    },
+    {
+    question: "Who directed th 1997 film Jackie Brown?",
+    answers: {
+    a: "Tim Burton",
+    b: "James Cameron",
+    c: "Steven Spielberg",
+    d: "Quentin Taratino",
+    },
+    correctAnswer: "d",
+    },
+    {
+    question:
         "How many times was Leonardo DiCaprio nominated for an Oscar before winning Best Actor in 2016",
-      a: "3",
-      b: "4",
-      c: "5",
-      d: "6",
+        answers: {
+    a: "3",
+    b: "4",
+    c: "5",
+    d: "6",
     },
-      correctAnswer: "d",
-    },
-    {
-      question: "Which social network is the film The Social Network based on?",
-      a: "Instagram",
-      b: "MySpace",
-      c: "Twitter",
-      d: "Facebook",
-    },
-      correctAnswer: "d",
+    correctAnswer: "d",
     },
     {
-      question: "Which sport is the subject of the 1993 film Cool Runnings?",
-      a: "Bobsleigh",
-      b: "Ice Hockey",
-      c: "Long Distance Running",
-      d: "Ice Skating",
+    question: "Which social network is the film The Social Network based on?",
+    answers: {
+    a: "Instagram",
+    b: "MySpace",
+    c: "Twitter",
+    d: "Facebook",
     },
-      correctAnswer: "a",
+    correctAnswer: "d",
     },
-  ];
+    {
+    question: "Which sport is the subject of the 1993 film Cool Runnings?",
+    answers: {
+    a: "Bobsleigh",
+    b: "Ice Hockey",
+    c: "Long Distance Running",
+    d: "Ice Skating",
+    },
+    correctAnswer: "a",
+    },
+];
 
-  function generateQuiz(
-    questions,
-    quizContainer,
-    resultsContainer,
-    submitButton
-  ) {
-    function showQuestions(questions, quizContainer) {
-      let output = [];
-      const answer;
+const quizContainer = document.getElementById("quiz");
+const results = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 
-      for(let i = 0; i < questions.length; i++){
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+	function showQuestions(questions, quizContainer) {
+    let output = [];
+    const answers;
+
+    for(let i = 0; i < questions.length; i++){
         answers = [];
         for(letter in questions[i].answers){
           answers.push(
             '<label>'
             + '<input type="radio" name="question'+i+'"value="'+letter+'">'
             + letter + ': '
-            + questions[i].answers{letter}
+            + questions[i].answers[letter]
             + '</label>'
           ) ;      
       }
-      output.push{
+      output.push(
         '<div>' + questions[i].question + '</div>'
         + '<div>' + answers.join("") + '</div>'
-);
-      }
+      );
+      
+    
     }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     function showResults(questions, quizContainer, resultsContainer) {}
 
     showQuestions(questions, quizContainer);
