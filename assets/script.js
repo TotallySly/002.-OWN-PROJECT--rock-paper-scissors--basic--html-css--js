de8d4cd7499188c2841acf5b712a89482747fb5e
TotallySly <07slyon@gmail.com>
2022-01-06 13:37:01 +0100

    Add TODOs to code

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
-const compChoice = document.getElementById("comp-choice");
-const playerChoice = document.getElementById("player-choice");
+const compChoiceDisplay = document.getElementById("comp-choice");
+const playerChoiceDisplay = document.getElementById("player-choice");
 const resultDisplay = document.getElementById("result");
+
+// TODO
+// Will need to change to class name if i plan to add more buttons. I.E GAME RULES/HARDER GAME (MAYBE)
+const possibleChoices = document.querySelectorAll("button");
+
+
+
+let playerChoice;
+
+
+// For each Button named possible choice, add an event listener when user clicks. When clicked, target the ID of the button and display this in Browser
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
+  playerChoice = e.target.id
+
+  // TODO
+  //Change to Font Awesome Icons
+  playerChoiceDisplay.innerHTML = playerChoice
+}))
+
+
+//Functions// 
