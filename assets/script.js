4c89c5a207013b03ef183e2644bf95bab927d9df
TotallySly <07slyon@gmail.com>
2022-01-06 11:46:37 +0100

    Add basic HTMl to page

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,1 @@
-const computerChoice = document.querySelector(".display-computer-choice");
-const result = document.getElementById("result");
-const possibleChoices = document.querySelector(".choiceBtn");
 
-possibleChoices.forEach((possibleChoice) =>
-  possibleChoice.addEventListener("click", function (event) {})
-);
