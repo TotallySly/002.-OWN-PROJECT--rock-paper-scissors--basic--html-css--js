85445867b9ee8e623a7f54287df5ec9a604de769
TotallySly <07slyon@gmail.com>
2022-01-06 10:42:03 +0100

    create global variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
-
+// Global Variables
+const userRock = document.querySelector("rock-icon");
+const userPaper = document.querySelector("paper-icon");
+const userScissors = document.querySelector("scissor-icon");
+const showCompChoice = document.querySelector("display-computer-choice");
+const userScore = document.querySelector("user-score");
+const compScore = document.querySelector("computer-score");
