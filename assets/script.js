32aa66c6c72ff12d6b0721c8e3c2534e55d32efe
TotallySly <07slyon@gmail.com>
2022-01-06 11:49:03 +0100

    Add global variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,3 @@
-
+const compChoice = document.getElementById("comp-choice");
+const playerChoice = document.getElementById("player-choice");
+const resultDisplay = document.getElementById("result");
