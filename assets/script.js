289796abfed6a9dc288cd452835395065fe4ad7c
TotallySly <07slyon@gmail.com>
2022-01-06 09:17:00 +0100

    Test javascript so computer choice is being shown to user

--- assets/script.js
+++ assets/script.js
@@ -5,4 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {});
 let userScore = 0;
 let computerScore = 0;
 
-const computerChoice = document.get;
+const userClickRock = document
+  .querySelector(".rock-icon")
+  .addEventListener("click", function () {
+    document.querySelector(
+      ".display-computer-choice"
+    ).innerHTML = `<i class="fas fa-hand-rock"></i>`;
+  });
+
+const userClickPaper = document
+  .querySelector(".paper-icon")
+  .addEventListener("click", function () {
+    document.querySelector(
+      ".display-computer-choice"
+    ).innerHTML = `<i class="fas fa-hand-paper"></i>`;
+  });
+
+const userClickScissor = document
+  .querySelector(".scissor-icon")
+  .addEventListener("click", function () {
+    document.querySelector(
+      ".display-computer-choice"
+    ).innerHTML = `<i class="fas fa-hand-scissors"></i>`;
+  });
